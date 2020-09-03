a3cccbbce51b3446d1f2b21807368babc75d62f3
jovan <jovanbapla@gmail.com>
2020-09-03 00:06:57 -0700

    stuff

--- src/index.js
+++ src/index.js
@@ -4,20 +4,26 @@ import "./index.css";
 
 function Header(){
   return(
+    <>
     <h1 id="title">ToolKit</h1>
+    <p id="intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Mauris porta ex eget feugiat efficitur. Nulla non libero dolor.</p>
+    </>
   );
 }
 function Input(){
   return(
-    <button>Start</button>
-  )
+    <div id="buttonWrapper">
+    <button id="beginButton">Rate Your Day</button>
+    </div>
+  );
 }
 
 function App(){
   return(
     <>
-    <Header/>,
-    <Input/>,
+    <Header/>
+    <Input/>
     </>
   );
 }
