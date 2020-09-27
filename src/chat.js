896a5f43f84c5d539d09661f45dad6847ea7cbab
jovan <jovanbapla@gmail.com>
2020-09-27 01:51:40 -0700

    server set up and client to server messages sending

---
+++ src/chat.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+import "./index.css";
+import "./grid.css";
+import "./index.js"
+
+function Send(){
+    
+
+    return(
+    //send socket data to server
+    console.log()
+           
+    )
+
+}
+
+export default Send;
+
--- src/index.js
+++ src/index.js
@@ -1,29 +1,90 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
+import SocketIOClient from "socket.io-client"
 import "./index.css";
+import "./grid.css";
+import "./chat.js"
 
-function Header(){
+
+const ENDPOINT = "http://127.0.0.1:4000";
+const socket = SocketIOClient(ENDPOINT);
+
+function Video(){
+    
+
+    let [message, changeMessage]= useState("")
+
+    function updateInput(event){
+      changeMessage(event.target.value)
+    }
+  
+    
+
+    function sendKey(event){
+      if(event.key==='Enter'){
+        console.log(message)
+        socket.emit("chat", {
+          message: message
+        });
+        // socket.on("chat", function(data){
+        //   console.log(data.message)
+        // })
+      }
+    }
+
+    function sendButton(event){
+     console.log(message)
+    }    
+   
+  
+  
+  
   return(
-    <>
-    <h1 id="title">ToolKit</h1>
-    <p id="intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Mauris porta ex eget feugiat efficitur. Nulla non libero dolor.</p>
-    </>
-  );
-}
-function Input(){
-  return(
-    <div id="buttonWrapper">
-    <button id="beginButton">Rate Your Day</button>
-    </div>
+    <div className="grid two-column">
+      <div id="video">
+        
+        <div className="grid four-column">
+            <button id="null">action</button>
+            <button id="null">action</button>
+            <button id="null">action</button>
+            <button id="null">action</button>
+        </div>
+        
+        <div id="iframe-container">          
+            <iframe id="video-player" src="" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+      
+      </div>
+      <div className="chat-section">
+            
+            <div id="chat-container">
+                <div id="chat">
+               
+                </div>   
+            
+            </div>
+           
+            <div id="message-area-container">
+                <div id="message-area">
+                    
+                    <input id="message" type="text" placeholder="message" onChange={updateInput} onKeyPress={sendKey}/>
+                    <button onClick={sendButton} id="send">send</button>
+                    {/* <input id="name" type="text" placeholder="Name" /> */}
+                </div>
+            </div>
+
+        </div>  
+    
+    
+    </div> 
+
   );
 }
 
 function App(){
   return(
     <>
-    <Header/>
-    <Input/>
+    <Video/>
     </>
   );
 }
